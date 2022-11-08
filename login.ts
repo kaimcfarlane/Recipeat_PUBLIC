c2d3be8f79d7766b51986d269f8104b7f7a8f6e8
kaimcfarlane <kaimcfarlane43@gmail.com>
2022-11-08 09:58:20 -0500

    Revert "Uploaded Current Recipeat Public Files"
    This reverts commit 0580e7542e780421cd78011993783e21fef400bf.

--- login.ts
+++
@@ -1,9 +0,0 @@
-console.log("Login typescript working")
-
-//We need to add a button that checks if hash matches inputted hash, if so the button will go to link 
-
-// var username = (<HTMLInputElement>document.getElementById("username")).value;
-// var password = (<HTMLInputElement> document.getElementById("password")).value;
-function logButton() {
-
-}
